8a21897a48cb7e888c63f71a3fdacb5ed3aab63c
touhid2025 <touhidbinamin@gmail.com>
2025-09-17 14:42:02 +0600

    modified name

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>hello I am Touhid.</h2>
+      <h2>hello I am Touhid Bin Amin.</h2>
     </div>
   );
 }
