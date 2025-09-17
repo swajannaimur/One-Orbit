be2ee3819af6f73e53a06db34cae75041e02888b
sazzad731 <sazzadho731@gmail.com>
2025-09-17 14:46:55 +0600

    [ADDED]: Navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <header className="max-w-7xl mx-auto">
+          <Navbar />
+        </header>
+        <main className="max-w-7xl mx-auto">{children}</main>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>hello</h2>
+      <h2>Hello</h2>
     </div>
   );
 }
