41a57f5b03ba210bb0e415ab7b6762ee062194f1
Ummae Habiba Jahan Aney <jahananey90@gmail.com>
2025-09-17 22:29:33 +0600

    [Added]:newsletter section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Footer from "./homepagesections/footer";
 import FaqSection from "./HomePageSections/FaqSection";
+import NewsletterSection from "./HomePageSections/NewsletterSection";
 
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, One-Orbit!</h1>
+      <NewsletterSection />
       <FaqSection />
       <Footer />
     </div>
