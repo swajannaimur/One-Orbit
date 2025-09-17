c7a43bd3c52f472a623754fa2e226b53b0778110
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-09-17 15:55:23 +0600

    yasin branch created

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h2>hello</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h2>hello I am yasin</h2>
+
+            
+        </div>
+	);
 }
