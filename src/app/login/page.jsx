3388ba9c495c7a3a3ba29758dac06be0378d5f63
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-09-18 12:51:32 +0600

    [MODIFIED]: cursor pointer added in login register button

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -46,7 +46,7 @@ export default function Login() {
                     {/* login button */}
                     <button
                         type="submit"
-                        className="w-full py-2 bg-blue-600 text-white font-semibold tracking-wider rounded-md
+                        className="w-full py-2 cursor-pointer bg-primary text-white font-semibold tracking-wider rounded-md
                  hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >
                         Login
