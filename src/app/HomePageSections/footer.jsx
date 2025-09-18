54754cfd3c6b7f8353830e6c2d4a6eb08a898456
Ummae Habiba Jahan Aney <jahananey90@gmail.com>
2025-09-18 22:28:48 +0600

    Updated footer

--- src/app/HomePageSections/footer.jsx
+++ src/app/HomePageSections/footer.jsx
@@ -1,52 +1,57 @@
+import Link from "next/link";
 import React from "react";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-teal-100 w-full py-12 px-4">
-      <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
-        <div className="font-bold text-lg tracking-wide text-[#064232]">
-          One-Orbit
+    <footer className="w-full bg-secondary text-black py-16 px-4">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
+        <div className="font-bold text-lg tracking-wide">
+          <Link href="/" className="text-2xl font-bold">
+            OneOrbit
+          </Link>
         </div>
         <nav className="flex flex-wrap gap-6 text-base">
           <a
             href="#about"
-            className="text-[#064232] hover:text-teal-700 transition"
+            className="hover:text-primary font-semibold transition"
           >
             About
           </a>
           <a
             href="#services"
-            className="text-[#064232] hover:text-teal-700 transition"
+            className="hover:text-primary font-semibold transition"
           >
             Services
           </a>
           <a
             href="#contact"
-            className="text-[#064232] hover:text-teal-700 transition"
+            className="hover:text-primary font-semibold transition"
           >
             Contact
           </a>
         </nav>
         <div className="flex gap-4">
           <a
-            href="https://facebook.com"
+            href="https://facebook.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-[#064232] hover:text-teal-700 transition"
+            className="hover:text-primary transition"
           >
-            Facebook
+            <FaFacebook size={25}></FaFacebook>
           </a>
           <a
-            href="https://linkedin.com"
+            href="https://linkedin.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-[#064232] hover:text-teal-700 transition"
+            className="hover:text-primary transition"
           >
-            LinkedIn
+            <FaLinkedin size={25}></FaLinkedin>
           </a>
         </div>
       </div>
-      <div className="text-center text-sm text-[#064232] mt-6">
+
+      <div className="text-center text-sm  mt-6">
         &copy; {new Date().getFullYear()} One-Orbit. All rights reserved.
       </div>
     </footer>
