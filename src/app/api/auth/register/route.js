cc408d794a05a9fcc689c2d7920cfd94c331f1e7
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-09-24 13:46:49 +0600

    [ADDED]: register post api created

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import clientPromise from "../../../../../lib/mongodb";
+
+
+// collection name
+const USERS_COLLECTION = "users";
+
+// to register as a new user we need to create POST request manually
+export const POST = async (request)=>{
+    try{
+        const { name ,email, password } = await request.json();
+
+        // connect with mongodb
+        const client = await clientPromise;
+        const db = client.db("AuthMongoNext"); // database name
+
+        // checking if the user already exists
+        const existingUser = await db.collection(USERS_COLLECTION).findOne({email});
+        if(existingUser) {
+            return new NextResponse("Email is already in use.", {status: 400});
+        }
+
+        // if the user is new , then continue further processes
+        // hash his/her password
+        const hashedPassword = await bcrypt.hash(password,5);
+
+        // now insert the new user to the database
+        await db.collection(USERS_COLLECTION).insertOne({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        return new NextResponse("User is Registered Successfully.", {status: 200});
+    }
+    catch(err){
+        console.error("Register error : ", err);
+        return new NextResponse("Internal Server Error", {status: 500});
+    }
+};
