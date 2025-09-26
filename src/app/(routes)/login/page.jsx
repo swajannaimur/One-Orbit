9dca6f46ed64e492cab0fa6419832f30b6a03feb
Ummae Habiba Jahan Aney <jahananey90@gmail.com>
2025-09-26 07:47:24 +0600

    Created the reusable social login component with Google and GitHub buttons

--- src/app/(routes)/login/page.jsx
+++ src/app/(routes)/login/page.jsx
@@ -1,11 +1,14 @@
-import LoginForm from '@/components/forms/LoginForm'
-import Link from 'next/link'
-import React from 'react'
+import LoginForm from "@/components/forms/LoginForm";
+import Social from "@/components/social";
+import Link from "next/link";
+import React from "react";
 
 export default function Login() {
-    return (
-        <div className='h-screen flex flex-col justify-center items-center'>
-            <LoginForm></LoginForm>
-        </div>
-    )
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <LoginForm></LoginForm>
+      <p className="mt-4 text-sm text-gray-600">Or login with</p>
+      <Social></Social>
+    </div>
+  );
 }
