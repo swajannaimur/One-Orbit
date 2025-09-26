2966a299970538d848b49d0970367fec0835580b
sazzad731 <sazzadho731@gmail.com>
2025-09-26 09:27:21 +0600

    [MODIFIED]: added countdown timer

--- src/app/(routes)/verify-otp/page.jsx
+++ src/app/(routes)/verify-otp/page.jsx
@@ -1,18 +1,30 @@
 "use client";
-import { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import OtpInput from "react-otp-input";
+import { signIn } from "next-auth/react";
+import Swal from "sweetalert2";
 
 export default function VerifyOTP() {
+  const router = useRouter();
+  const params = useSearchParams();
+  const email = params.get("email");
+  const password = localStorage.getItem("password");
+  const [sending, setSending] = useState(false);
+
   const [otp, setOtp] = useState("");
+  const [countdown, setCountdown] = useState(300); // 5 minutes
 
-  const handleChange = (value) => {
-    setOtp(value);
-  };
-
-  const handleVerify = () => {
-    alert(`OTP entered: ${otp}`);
-  };
-
+  // Countdown timer
+  useEffect(() => {
+    const timer =
+      countdown > 0 &&
+      setInterval(() => setCountdown((prev) => prev - 1), 1000);
+    return () => clearInterval(timer);
+  }, [ countdown ]);
+  
+  const minutes = String(Math.floor(countdown / 60)).padStart(2, "0");
+  const seconds = String(countdown % 60).padStart(2, "0");
   return (
     <div className="flex flex-col w-full max-w-[512px] flex-1 mx-auto min-h-screen pt-20">
       <h2 className="text-3xl font-bold text-center pb-3">Enter the code</h2>
@@ -49,7 +61,7 @@ export default function VerifyOTP() {
       </div>
 
       <p className="text-primary text-sm pt-5 text-center">
-        Code expires in 00:59
+        Code expires in {minutes}:{seconds}
       </p>
     </div>
   );
