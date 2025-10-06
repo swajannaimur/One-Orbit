dca62f7d2172fc46d80f006289f79ae6ab1d447f
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-10-06 21:30:27 +0600

    [DONE]: Basic Client Dashboard Done

--- src/app/(routes)/dashboard/layout.jsx
+++ src/app/(routes)/dashboard/layout.jsx
@@ -1,15 +1,19 @@
 import Link from 'next/link'
 import React from 'react'
 import { FaHome, FaUser, FaCog, FaChartBar } from 'react-icons/fa'
+import { FiInbox, FiCreditCard } from "react-icons/fi";
+import { HiOutlineViewGrid } from "react-icons/hi";
+
 
 export default function DashboardLayout({ children }) {
     return (
-        <div className="flex min-h-screen bg-gray-100 py-16">
+        <div className="flex min-h-screen mt-20">
 
             {/* Sidebar */}
             <div className="w-64 bg-white shadow-lg p-5">
                 <Link href="/" className="text-2xl font-bold text-blue-600 mb-8">OneOrbit</Link>
 
+                {/* dashboard links */}
                 <ul className="space-y-4 mt-4">
                     <li>
                         <Link
@@ -17,6 +21,40 @@ export default function DashboardLayout({ children }) {
                             className="flex items-center gap-3 p-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition"
                         >
                             <FaHome /> Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/dashboard/"
+                            className="flex items-center gap-3 p-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition"
+                        >
+                            <HiOutlineViewGrid /> Overview
+                        </Link>
+                    </li>
+                    
+                    <li>
+                        <Link
+                            href="/dashboard/reports"
+                            className="flex items-center gap-3 p-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition"
+                        >
+                            <FaChartBar /> Reports
+                        </Link>
+                    </li>
+                   
+                    <li>
+                        <Link
+                            href="/dashboard/inbox"
+                            className="flex items-center gap-3 p-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition"
+                        >
+                            <FiInbox /> Inbox
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            href="/dashboard/payment"
+                            className="flex items-center gap-3 p-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition"
+                        >
+                            <FiCreditCard /> Payment
                         </Link>
                     </li>
                     <li>
@@ -35,16 +73,9 @@ export default function DashboardLayout({ children }) {
                             <FaCog /> Settings
                         </Link>
                     </li>
-                    <li>
-                        <Link
-                            href="/dashboard/reports"
-                            className="flex items-center gap-3 p-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition"
-                        >
-                            <FaChartBar /> Reports
-                        </Link>
-                    </li>
                 </ul>
-            </div>  
+
+            </div>
 
             {/* Main Content */}
             <div className="flex-1">
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -71,9 +71,9 @@ export default function Navbar() {
         <>
             <nav className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${isScrolled
                 ? 'bg-white/80 dark:bg-gray-900/80 backdrop-blur-xl shadow-lg'
-                : 'bg-transparent'
+                : 'bg-black'
                 }`}>
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="w-11/12 mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16 lg:h-20">
 
                         {/* Logo Section */}
