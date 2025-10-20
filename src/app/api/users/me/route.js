a48df58e8b2e835fa3125a8a2d94e9ebb5cb986b
Ummae Habiba Jahan Aney <jahananey90@gmail.com>
2025-10-20 11:33:31 +0600

    Added alldevelopers,page and developers details page

--- src/app/api/users/me/route.js
+++ src/app/api/users/me/route.js
@@ -1,35 +1,4 @@
-// import { getServerSession } from "next-auth";
-// import clientPromise from "@/lib/mongodb";
-// import { authOptions } from "../../auth/[...nextauth]/route";
 
-// export async function GET() {
-//   try {
-//     const session = await getServerSession(authOptions);
-//     if (!session?.user?.email) {
-//       return new Response(JSON.stringify({ error: "Unauthorized" }), {
-//         status: 401,
-//         headers: { "Content-Type": "application/json" },
-//       });
-//     }
-
-//     const client = await clientPromise;
-//     const db = client.db(process.env.DB_NAME);
-//     const user = await db
-//       .collection("users-data")
-//       .findOne({ email: session.user.email }, { projection: { password: 0 } });
-
-//     return new Response(JSON.stringify({ user }), {
-//       status: 200,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   } catch (err) {
-//     console.error("/api/users/me error", err);
-//     return new Response(JSON.stringify({ error: "Server error" }), {
-//       status: 500,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   }
-// }
 
 
 import { getServerSession } from "next-auth";
@@ -80,6 +49,7 @@ export async function GET() {
       facebook: user.facebook || "",
       portfolio: user.portfolio || "",
       resume: user.resume || "",
+      qna: user.qna || {},
     };
 
     return new Response(JSON.stringify({ user: safeUser }), {
