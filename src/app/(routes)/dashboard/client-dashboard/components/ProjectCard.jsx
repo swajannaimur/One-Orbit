f0ed37eb5dee6fbeb1a65979de4905f73c4b8377
Swajan Naimur <swajannaimur@gmail.com>
2025-10-31 10:02:05 +0600

    Developer complete function done

--- src/app/(routes)/dashboard/client-dashboard/components/ProjectCard.jsx
+++ src/app/(routes)/dashboard/client-dashboard/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import DetailsAccordion from './DetailsAccordion'
 
 export default function ProjectCard({ project }) {
     return (
-        <div className="rounded-lg p-4 shadow-sm hover:shadow-md transition duration-200 bg-white flex flex-col md:flex-row md:justify-between md:items-start gap-4">
+        <div className="rounded-lg p-4 shadow-sm hover:shadow-md transition duration-200 bg-white flex flex-col md:flex-row md:justify-between md:items-start gap-4 dark-bg">
 
             {/* Left Side - Project Info */}
             <ProjectInfo project={project}></ProjectInfo>
